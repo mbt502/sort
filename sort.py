427f74917de180248a36a28fe7cc5cdfec25fb12
mbt502 <41120146+mbt502@users.noreply.github.com>
2019-08-10 22:32:20 +0700

    Add files via upload

--- sort.py
+++ sort.py
@@ -1,27 +1,75 @@
-begin = open('begin.txt','r')
-digit = open('digit.txt','w')
-alpha = open('alpha.txt','w')
-code = open('code.txt','w')
-countnum,countalpha,countcode = 0,0,0
+begin = open('begin.txt','r')#чтение файла и запись в список
 s = begin.read().split()
-for i in range(len(s)):
-    if s[i].isdigit() == True and s[i].isalpha() == False:
-        digit.write(s[i])
-        digit.write('\n')
-        countnum += 1
-    elif s[i].isalpha() == True and s[i].isdigit() == False:
-        alpha.write(s[i])
-        alpha.write('\n')
-        countalpha += 1   
-    else:
-        code.write(s[i])
-        code.write('\n')
-        countcode += 1
-alpha.write(str(countalpha))
-digit.write(str(countnum))
-code.write(str(countcode))
 begin.close()
-digit.close()
-alpha.close()
-code.close()
 
+
+a = []
+b = []
+c = []
+d = []
+e = []
+
+
+for i in s:#первоначальная сортировка на слова, цифры и слова со знаками
+    if i.isdigit() and not(i.isalpha()):
+        a.append(i)    
+    elif i.isalpha() and not(i.isdigit()):
+        b.append(i)
+    elif i.isalpha and i.isdigit:
+        d.append(i)
+        
+for i in d:#удаление знаков и формирование двойного списка слов
+    for j in range(len(i)):
+        if not(i[j].isalnum()):
+            i = i.replace(i[j],' ')
+    c.append(i.split())
+d.clear()
+
+
+for i in c:#разбиение двойного списка на два одинарных
+    for j in i:
+        if len(i)>1:
+            d.append(j)
+        else:
+            e.append(j)
+c.clear()
+
+
+for i in d:#сортировка оставшихся элементов
+    if i.isdigit() and not(i.isalpha()):
+        a.append(i)    
+    elif i.isalpha() and not(i.isdigit()):
+        b.append(i)
+    else:
+        c.append(i)
+for i in e:
+    if i.isdigit() and not(i.isalpha()):
+        a.append(i)    
+    elif i.isalpha() and not(i.isdigit()):
+        b.append(i)
+    else:
+        c.append(i)
+d.clear()
+e.clear()
+
+
+a.sort()
+b.sort()
+c.sort()
+
+
+res = open('begin.txt','a')#запись в файл
+res.write('\n')
+res.write('\n')
+for i in a:
+    res.write(i)
+    res.write('\n')
+res.write('\n')
+for i in b:
+    res.write(i)
+    res.write('\n')
+res.write('\n')
+for i in c:
+    res.write(i)
+    res.write('\n')
+res.close()
